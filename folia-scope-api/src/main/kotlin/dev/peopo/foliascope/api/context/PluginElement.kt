2d21d48fa2ea5d4e2bc463b3f0ae4c310346b190
mertunctuncer <mertunctuncer@gmail.com>
2024-02-02 03:12:36 +0300

    Context element impl

--- folia-scope-api/src/main/kotlin/dev/peopo/foliascope/api/context/PluginElement.kt
+++ folia-scope-api/src/main/kotlin/dev/peopo/foliascope/api/context/PluginElement.kt
@@ -1,5 +1,6 @@
 package dev.peopo.foliascope.api.context
 
+import kotlinx.coroutines.CoroutineScope
 import org.bukkit.plugin.Plugin
 import kotlin.coroutines.CoroutineContext
 
@@ -9,3 +10,6 @@ interface PluginElement : CoroutineContext.Element{
     override val key: CoroutineContext.Key<*> get() = Key
     companion object Key: CoroutineContext.Key<PluginElement>
 }
+
+fun CoroutineContext.getPlugin() = this[PluginElement]?.plugin ?: throw IllegalStateException("FoliaDispatcher can only be used within a plugin context")
+fun CoroutineScope.getPlugin() = this.coroutineContext.getPlugin()
---
+++ folia-scope-core/src/main/kotlin/dev/peopo/foliascope/core/context/PluginElementImpl.kt
@@ -0,0 +1,7 @@
+package dev.peopo.foliascope.core.context
+
+
+import dev.peopo.foliascope.api.context.PluginElement
+import org.bukkit.plugin.Plugin
+
+class PluginElementImpl(override val plugin: Plugin) : PluginElement
