a4188c751f410766d0b93f911b29647ee0afb390
mertunctuncer <mertunctuncer@gmail.com>
2024-02-02 13:30:14 +0300

    Added custom exception

---
+++ folia-scope-api/src/main/kotlin/dev/peopo/foliascope/api/util/IllegalContextException.kt
@@ -0,0 +1,7 @@
+package dev.peopo.foliascope.api.util
+
+
+/**
+ * Exception thrown when the action can't be performed due to a lacking element
+ */
+class IllegalContextException(message: String) : Exception(message)
